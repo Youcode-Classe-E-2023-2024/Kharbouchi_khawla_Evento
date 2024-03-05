41752233568df06932ffc7760aa7d00ea3ad69e3
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-03-05 09:15:08 +0100

    page home

--- Evento/resources/views/home.blade.php
+++ Evento/resources/views/home.blade.php
@@ -1,13 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Home</title>
+    <style>
+
+    </style>
 </head>
+
 <body>
-    
+
+    <body>
+
+        <div style="background:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(https://t3.ftcdn.net/jpg/04/38/18/48/360_F_438184846_1BzROkQWk4Ffwf5wHgtemCLhdmoSugYl.jpg) no-repeat center;background-size:cover"
+        class="py-52 px-1 md:px-8 text-center relative text-white font-bold text-2xl md:text-3xl overflow-auto">
+        <h1 class="pb-4">Search for Events</h1>
+        <div class="w-11/12 md:w-3/4 lg:max-w-3xl m-auto">
+            <div class="relative z-30 text-base text-black"><input type="text" value="" placeholder="Keyword" class="mt-2 shadow-md focus:outline-none rounded-2xl py-3 px-6 block w-full">
+                <div
+                    class="text-left absolute top-10 rounded-t-none rounded-b-2xl shadow bg-white divide-y w-full max-h-40 overflow-auto">
+                </div>
+            </div>
+        </div>
+    </div>
+            
+                 <script></script>
 </body>
+
 </html>
--- Evento/resources/views/organisateur/dashorganis.blade.php
+++ Evento/resources/views/organisateur/dashorganis.blade.php
@@ -147,87 +147,13 @@
                 </div>
             </div>
 
-            <!-- ======================= Cards ================== -->
-            <div class="cardBox">
-                <div class="card">
-                    <div>
-                        <div class="numbers">1,504</div>
-                        <div class="cardName">Daily Views</div>
-                    </div>
-
-                    <div class="iconBx">
-                        <ion-icon name="eye-outline"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <div>
-                        <div class="numbers">80</div>
-                        <div class="cardName">Sales</div>
-                    </div>
-
-                    <div class="iconBx">
-                        <ion-icon name="cart-outline"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <div>
-                        <div class="numbers">284</div>
-                        <div class="cardName">Comments</div>
-                    </div>
-
-                    <div class="iconBx">
-                        <ion-icon name="chatbubbles-outline"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <div>
-                        <div class="numbers">$7,842</div>
-                        <div class="cardName">Earning</div>
-                    </div>
-
-                    <div class="iconBx">
-                        <ion-icon name="cash-outline"></ion-icon>
-                    </div>
-                </div>
-            </div>
-
             <!-- ================= New Customers ================ -->
 
             <div class="px-3 py-4 flex justify-center">
                 <div class="title" style="width: 100%; text-align: center; margin-bottom: 40px;margin-top:40px;">
-                    <h2>Clients</h2>
+                    <h2>Events</h2>
                 </div>
-                <table class="w-full text-md bg-white shadow-md rounded mb-4">
-                    <tbody>
-                        <tr class="border-b">
-                            <th class="text-left p-3 px-5">Name</th>
-                            <th class="text-left p-3 px-5">Email</th>
-                            <th class="text-left p-3 px-5">Role</th>
-                            <th></th>
-                        </tr>
-                        <tr class="border-b hover:bg-orange-100 bg-gray-100">
-                            <td class="p-3 px-5">
-                                <p>khawla</p>
-                            </td>
-                            <td class="p-3 px-5">
-                                <p>khawla@gmail.com</p>
-                            </td>
-                            <td class="p-3 px-5">
-                                <select value="user.role" class="bg-transparent">
-                                    <option value="user">user</option>
-                                    <option value="admin">admin</option>
-                                </select>
-                            </td>
-                            <td class="p-3 px-5 flex justify-end"><button type="button"
-                                    class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Save</button><button
-                                    type="button"
-                                    class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Delete</button>
-                            </td>
-                        </tr>
-                </table>
+
             </div>
 
         </div>
