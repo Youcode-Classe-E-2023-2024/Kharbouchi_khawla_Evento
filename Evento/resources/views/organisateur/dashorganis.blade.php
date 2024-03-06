8ac247e7748b1a335449e44cbff70f980a1d460c
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-03-06 23:25:00 +0100

    front de page organisateur

--- Evento/resources/views/organisateur/dashorganis.blade.php
+++ Evento/resources/views/organisateur/dashorganis.blade.php
@@ -9,71 +9,120 @@
     <!-- ======= Styles ====== -->
     <link rel="stylesheet" href="assets/css/style.css">
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Righteous&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap');
 
-        table {
-            border-collapse: collapse;
+        .ana {
+            gap: 20px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+        }
+
+        .containe {
+            backdrop-filter: blur(16px) saturate(180%);
+            -webkit-backdrop-filter: blur(16px) saturate(180%);
+            background-color: rgba(17, 25, 40, 0.25);
+            border-radius: 12px;
+            border: 1px solid rgba(255, 255, 255, 0.125);
+            padding: 38px;
+            filter: drop-shadow(0 30px 10px rgba(0, 0, 0, 0.125));
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            width: 30%;
+        }
+
+        .wrapper {
             width: 100%;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            border-radius: 8px;
-            overflow: hidden;
+            height: 100%;
         }
 
-        th {
-            background-color: #f8f9fa;
-            text-align: left;
-            padding: 12px;
-            font-weight: 600;
-        }
-
-        tr:nth-child(even) {
-            background-color: #f2f2f2;
-        }
-
-        td {
-            padding: 12px;
-            border-bottom: 1px solid #eaeaea;
-        }
-
-        input,
-        select {
-            border: 1px solid #ddd;
-            border-radius: 4px;
-            padding: 8px;
+        .banner-image {
+            background-image: url(https://images.unsplash.com/photo-1641326201918-3cafc641038e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80);
+            background-position: center;
+            background-size: cover;
+            height: 200px;
             width: 100%;
-            box-sizing: border-box;
-            transition: border-color 0.3s;
+            border-radius: 12px;
+            border: 1px solid rgba(255, 255, 255, 0.255)
         }
 
-        input:focus,
-        select:focus {
-            outline: none;
-            border-color: #007bff;
+        h1 {
+            font-family: 'Righteous', sans-serif;
+            color: rgba(255, 255, 255, 0.98);
+            text-transform: uppercase;
+            font-size: 25px;
+
         }
 
-        button {
-            cursor: pointer;
+        p {
+            color: #fff;
+            font-family: 'Lato', sans-serif;
+            text-align: center;
+            font-size: 0.8rem;
+            line-height: 150%;
+            letter-spacing: 2px;
+            text-transform: uppercase;
+        }
+
+        .button-wrapper {
+            margin-top: 18px;
+        }
+
+        .btn {
             border: none;
-            border-radius: 4px;
-            padding: 8px 16px;
-            transition: background-color 0.3s;
+            padding: 12px 24px;
+            border-radius: 24px;
+            font-size: 12px;
+            font-size: 0.8rem;
+            letter-spacing: 2px;
+            cursor: pointer;
         }
 
-        button:hover {
-            opacity: 0.8;
+        .btn+.btn {
+            margin-left: 10px;
         }
 
-        .bg-blue-500 {
-            background-color: #007bff;
-            color: white;
+        .outline {
+            background: transparent;
+            color: rgba(0, 212, 255, 0.9);
+            border: 1px solid rgba(0, 212, 255, 0.6);
+            transition: all .3s ease;
+
         }
 
-        .bg-red-500 {
-            background-color: #dc3545;
-            color: white;
+        .outline:hover {
+            transform: scale(1.125);
+            color: rgba(255, 255, 255, 0.9);
+            border-color: rgba(255, 255, 255, 0.9);
+            transition: all .3s ease;
         }
 
-        tr:hover {
-            background-color: #f8f9fa;
+        .fill {
+            background: rgba(0, 212, 255, 0.9);
+            color: rgba(255, 255, 255, 0.95);
+            filter: drop-shadow(0);
+            font-weight: bold;
+            transition: all .3s ease;
+        }
+
+        .fill:hover {
+            transform: scale(1.125);
+            border-color: rgba(255, 255, 255, 0.9);
+            filter: drop-shadow(0 10px 5px rgba(0, 0, 0, 0.125));
+            transition: all .3s ease;
+        }
+
+        input {
+            background-color: #eee;
+            border: none;
+            padding: 12px 15px;
+            margin: 8px 0;
+            width: 100%;
         }
     </style>
 </head>
@@ -118,7 +167,6 @@
                 </li>
             </ul>
         </div>
-
         <!-- ========================= Main ==================== -->
         <div class="main">
             <div class="topbar">
@@ -137,20 +185,77 @@
                     <img src="assets/imgs/customer01.jpg" alt="">
                 </div>
             </div>
-
             <!-- ================= New Customers ================ -->
 
             <div class="px-3 py-4 flex justify-center">
                 <div class="title" style="width: 100%; text-align: center; margin-bottom: 40px;margin-top:40px;">
-                    <h2>Events</h2>
+                    <h2 style="margin-bottom: 40px;">Events</h2>
+                    <button class="btn fill" onclick="document.getElementById('demo-modal').showModal();">Add
+                        Event</button>
                 </div>
-
+                <div class="ana">
+                    <div class="containe">
+                        <div class="wrapper">
+                            <div class="banner-image"> </div>
+                            <h1> Toyota Supra</h1>
+                            <p>Lorem ipsum dolor sit amet, <br />
+                                consectetur adipiscing elit.</p>
+                        </div>
+                        <div class="button-wrapper">
+                            <button class="btn outline">DETAILS</button>
+                            <button class="btn fill">MODIFIER</button>
+                        </div>
+                    </div>
+                    <div class="containe">
+                        <div class="wrapper">
+                            <div class="banner-image"> </div>
+                            <h1> Toyota Supra</h1>
+                            <p>Lorem ipsum dolor sit amet, <br />
+                                consectetur adipiscing elit.</p>
+                        </div>
+                        <div class="button-wrapper">
+                            <button class="btn outline">DETAILS</button>
+                            <button class="btn fill">BUY NOW</button>
+                        </div>
+                    </div>
+                    <div class="containe">
+                        <div class="wrapper">
+                            <div class="banner-image"> </div>
+                            <h1> Toyota Supra</h1>
+                            <p>Lorem ipsum dolor sit amet, <br />
+                                consectetur adipiscing elit.</p>
+                        </div>
+                        <div class="button-wrapper">
+                            <button class="btn outline">DETAILS</button>
+                            <button class="btn fill">BUY NOW</button>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
 
     </div>
 
+    </div>
+    <!-- =========== Modal =========  -->
+    <dialog id="demo-modal"
+    style="backdrop-filter: blur(16px) saturate(180%);-webkit-backdrop-filter: blur(16px) saturate(180%);
+    background-color: rgba(17, 25, 40, 0.25);border-radius: 12px;border: 1px solid rgba(255, 255, 255, 0.125);
+    padding: 38px;filter: drop-shadow(0 30px 10px rgba(0, 0, 0, 0.125)); position: fixed; top: 50%; left: 50%;
+    transform: translate(-50%, -50%);">
+    <button onclick="document.getElementById('demo-modal').close();">x</button>
+    <form action="">
+        <input type="text" placeholder="title" />
+        <textarea placeholder="Add Description" tabindex="5" required
+            style="background-color: #eee;border: none;padding: 12px 15px;margin: 8px 0;width: 100%;"></textarea>
+        <input type="number" placeholder="Places" />
+        <input type="number" placeholder="Prix DH" />
+        <input type="text" placeholder="Lieu" />
+        <input type="date" placeholder="date finale" />
+        <button type="submit" class="btn fill" style="margin-top: 20px; ">Save</button>
+    </form>
+</dialog>
     <!-- =========== Scripts =========  -->
     <script src="assets/js/main.js"></script>
 
